e5ddca539adf93fc61183ad2fa88193b614273c8
joaquinriveros <joakocarp19@gmail.com>
2025-07-31 13:21:49 -0300

    Cambios en ProductPipelineApplication.java

---
+++ product-pipeline/src/main/java/com/pipeline/ProductPipelineApplication.java
@@ -0,0 +1,11 @@
+package com.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPipelineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPipelineApplication.class, args);
+    }
+}
